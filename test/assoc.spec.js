9ffc3f3f8e9692219100f0255e5c67e22ce60645
Geoffrey <geoffrey@tourconnect.com>
2016-06-19 12:23:51 -0500

    Adds composability test case for assoc

--- test/assoc.spec.js
+++ test/assoc.spec.js
@@ -1,7 +1,8 @@
 /* eslint-env mocha */
 
 import chai from 'chai';
-import { assoc } from '../src/index';
+import { assoc, prop } from '../src/index';
+import { compose } from 'ramda';
 import 'babel-polyfill';
 import { Map } from 'immutable';
 chai.should();
@@ -10,5 +11,14 @@ describe('assoc', () => {
   it('should set a value onto a given key for either an Immutable data structure or a JS Object', () => {
     assoc('a', 'test', {}).should.deep.equal({ a: 'test' });
     assoc('a', 'test', new Map()).toJS().should.deep.equal({ a: 'test' });
+  });
+
+  it('should be composable', () => {
+    const tester = compose(
+      prop('b'),
+      assoc('b', 'b'),
+      assoc('a', 'a')
+    );
+    tester(new Map()).should.equal('b');
   });
 });
